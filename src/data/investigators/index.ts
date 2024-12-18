20a3d51790cbafed93e603fd82c4dc133df7893c
Vladimir Yazykov <neizerth@gmail.com>
2024-12-18 12:10:58 +0300

    added investigator decks

--- src/data/investigators/index.ts
+++ src/data/investigators/index.ts
@@ -1,9 +1,9 @@
 import * as campaigns from './campaigns';
 import promo from './promo.json';
-import challenge from './challenge.json';
+import decks from './decks.json';
 
 export default [
   ...Object.values(campaigns),
   promo,
-  challenge
+  decks
 ]
