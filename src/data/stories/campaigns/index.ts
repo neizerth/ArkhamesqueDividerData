b2c5babe24d379914b5c182e111b3c475ababcda
Vladimir Yazykov <neizerth@gmail.com>
2025-04-15 06:58:27 +0300

    added tdc

--- src/data/stories/campaigns/index.ts
+++ src/data/stories/campaigns/index.ts
@@ -1,23 +1,13 @@
-import core from './core.json';
-import dwl from './dwl.json';
-import ptc from './ptc.json';
-import tcu from './tcu.json';
-import tfa from './tfa.json';
-import tic from './tic.json';
-import tde from './tde.json';
-import eoe from './eoe.json';
-import tsk from './tsk.json';
-import fhv from './fhv.json';
+import core from "./core.json";
+import dwl from "./dwl.json";
+import ptc from "./ptc.json";
+import tcu from "./tcu.json";
+import tfa from "./tfa.json";
+import tic from "./tic.json";
+import tde from "./tde.json";
+import eoe from "./eoe.json";
+import tsk from "./tsk.json";
+import fhv from "./fhv.json";
+import tdc from "./tdc.json";
 
-export default [
-  core,
-  dwl,
-  ptc,
-  tcu,
-  tfa,
-  tic,
-  tde,
-  eoe,
-  tsk,
-  fhv
-]
+export default [core, dwl, ptc, tcu, tfa, tic, tde, eoe, tsk, fhv, tdc];
